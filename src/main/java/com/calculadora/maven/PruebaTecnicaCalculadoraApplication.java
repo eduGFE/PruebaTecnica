306c9baef6d28f18a6225835f737988a4a79753a
EDU\edu21 <edu21061991@gmail.com>
2022-07-04 18:44:28 +0200

    Se sube el proyecto maven vacio

---
+++ src/main/java/com/calculadora/maven/PruebaTecnicaCalculadoraApplication.java
@@ -0,0 +1,13 @@
+package com.calculadora.maven;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PruebaTecnicaCalculadoraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PruebaTecnicaCalculadoraApplication.class, args);
+	}
+
+}
